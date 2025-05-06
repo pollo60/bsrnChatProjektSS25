55bd7aa5f8dca4b7606bcba485d4231510fbab23
Altay Hennig <server.zehntel0s@icloud.com>
2025-05-06 17:32:59 +0200

    einfach bissle cli

---
+++ cli.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+def main():
+    weiter = True
+
+    while weiter:
+        print("Wilkommen! Was moechtest Du tun?")
+        print(">> 1 << WHO")
+        print(">> 2 << EXIT")
+
+    try:
+        auswahl = int(input("Deine Wahl: "))
+    except ValueError:
+        print("Bitte eine gueltige Zahl eingeben")
+        continue
+
+    if auswahl == 1
+    WHO()
+    
+
+    if auswahl == 2
+
+    print("Programm beendet.")
+
+    weiter = False
+else: 
+ print("ungueltige Auswahl")
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+def WHO():
+    print "socket.ShowAll()"
