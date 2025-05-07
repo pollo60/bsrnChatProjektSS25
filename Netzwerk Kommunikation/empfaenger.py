cd2e8ce5bb44ecf67b0c1777520d8a0d7c10dbfb
Altay Hennig <server.zehntel0s@icloud.com>
2025-05-07 14:56:45 +0200

    was ist netzwerk Kommunikation

---
+++ Netzwerk Kommunikation/empfaenger.py
@@ -0,0 +1,21 @@
+# empfaenger.py
+
+
+import socket
+
+PORT = 5000
+
+BUFFER_SIZE = 1024
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind("0.0.0.0", PORT)
+
+
+print(f"Dein Port ist: {PORT}. Warte auf Nachrichten...")
+
+while TRUE:
+    daten, addr = socket.recvfrom(BUFFER_SIZE)
+    nachricht = daten.decode("utf-8")
+    print(f"Nachricht von {addr[0]}:{addr[1]} -> {nachricht}")
+    
