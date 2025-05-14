e862953c432369ced982e6fa9ab14de7e4fe3fd2
Fabiankohlmann <fabiankohlmann@gmx.de>
2025-05-14 19:50:48 +0200

    Add files via upload

---
+++ dokumentation/bsrn_project/src/main.py
@@ -0,0 +1,14 @@
+def berechne_summe(a, b):
+    """Berechnet die Summe zweier Zahlen.
+    
+    Args:
+        a (int): Erste Zahl.
+        b (int): Zweite Zahl.
+        
+    Returns:
+        int: Summe von a und b.
+    """
+    return a + b
+
+if __name__ == "__main__":
+    print(berechne_summe(3, 5))  # Testaufruf
