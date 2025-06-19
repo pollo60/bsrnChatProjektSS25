9ad8dc189c0923453daf869ec1c971c4ae4cc4ed
Eisi <lea.eiswirth@web.de>
2025-06-19 18:03:38 +0200

    main_queue

--- Ayman_Discovery/eigene_prozess_discovery_beta/main.py
+++ Ayman_Discovery/eigene_prozess_discovery_beta/main.py
@@ -4,17 +4,23 @@ from config_utility import config_startup, get_contacts_path
 from ui import start_cli
 from discovery_daemon_queue import run_discovery
 from message_listener_queue import listen_for_messages
+from network_process import send_slcp_broadcast
 
 def main():
     config_path, auto_mode, handle, port, whoisport, ip, broadcast_ip = config_startup()
     contacts_path = get_contacts_path()
 
-    queue = Queue()
+    input_queue = Queue()   # Befehle von UI an Netzwerkprozess
+    output_queue = Queue()  # Netzwerk-Nachrichten an UI
 
-    discovery_proc = Process(target=run_discovery, args=(config_path, queue))
-    receiver_proc = Process(target=listen_for_messages, args=(port, queue))
+    # Prozesse starten
+    discovery_proc = Process(target=run_discovery, args=(config_path, output_queue))
+    receiver_proc = Process(target=listen_for_messages, args=(port, output_queue))
+    network_proc = Process(target=send_slcp_broadcast, args=(input_queue, whoisport, broadcast_ip))
+
     discovery_proc.start()
     receiver_proc.start()
+    network_proc.start()
 
     print("[DEBUG] Prozesse gestartet")
 
@@ -27,15 +33,19 @@ def main():
             config_path=config_path,
             contacts_path=contacts_path,
             broadcast_ip=broadcast_ip,
-            message_queue=queue
+            message_queue=output_queue,
+            input_queue=input_queue
         )
     except KeyboardInterrupt:
         print("[ABBRUCH] Strg+C erkannt")
     finally:
         discovery_proc.terminate()
         receiver_proc.terminate()
+        network_proc.terminate()
+
         discovery_proc.join()
         receiver_proc.join()
+        network_proc.join()
         print("[ENDE] Prozesse gestoppt.")
 
 if __name__ == "__main__":
